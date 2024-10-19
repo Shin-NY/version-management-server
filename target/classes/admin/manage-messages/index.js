8c2368dcbe6a6e7e53284ec42a8f3a2e1e992383
SanNakji <wngyehs99@gmail.com>
2024-10-19 15:31:41 +0900

    [remove] target 폴더는 intellji의 부산물로 생긴 폴더로 삭제/ .idea폴더는 intellji 사용을 위해 필요한 폴더

--- target/classes/admin/manage-messages/index.js
+++
@@ -1,9 +0,0 @@
-import { logout, redirectIfLoggedOut } from '../utils.js';
-
-const main = async () => {
-  const button = document.getElementById('logout_button');
-  button.onclick = logout;
-};
-
-redirectIfLoggedOut();
-window.onload = main;
--- target/classes/target/classes/src/agent_version/agent_version.constants.ts
+++
@@ -1,2 +0,0 @@
-export const UPLOADED_AGENT_DIR_NAME = 'uploaded_agent';
-export const ARCHIVED_AGENT_DIR_NAME = 'archived_agent';
