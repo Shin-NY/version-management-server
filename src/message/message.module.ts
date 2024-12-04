3f79ae07f24a4593a8055fe85525ac117fb7cb85
SanNakji <wngyehs99@gmail.com>
2024-12-04 23:27:23 +0900

    [Feat] 메세지 readstatus가 빠진것 추가

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -4,9 +4,10 @@ import { MessagesController } from './message.controller';
 import { MessagesService } from './message.service';
 import { Message } from './entities/message_info.entity';
 import { MessageSend } from './entities/message_send.entity';
+import { MessageReadStatus } from './entities/message_read_status.entity'; // 새로 추가된 엔티티 임포트
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message, MessageSend])], 
+  imports: [TypeOrmModule.forFeature([Message, MessageSend, MessageReadStatus])], // MessageReadStatus 추가
   controllers: [MessagesController],
   providers: [MessagesService],
 })
